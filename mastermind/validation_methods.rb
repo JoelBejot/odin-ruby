99dbf2167ebfb77f5aea46bc68d0bc66876a404d
JoelBejot <61334576+JoelBejot@users.noreply.github.com>
2023-02-13 20:52:58 -0700

    Update validation_methods.rb
    Copy validate_num from tic-tac-toe

--- mastermind/validation_methods.rb
+++ mastermind/validation_methods.rb
@@ -15,4 +15,19 @@ def validate_boolean(answer)
       puts ""
     end
   end
+  
+  def validate_num
+    answer = "invalid"
+    while answer == "invalid"
+      puts "In which space would you like to make your move?(1-9)"
+      input = gets.chomp.to_i
+      puts ""
+      any = @total_moves.any? { |el| el == input }
+      if input.between?(1, 9) && any == false
+        @total_moves << input
+        return input
+        answer = "valid"
+      end
+    end
+  end
 end
